ec75794b1b4cb5f206f11367f2e1fb468f769846
Rashmi <rashmiagar@gmail.com>
2016-07-31 14:07:37 +0530

    Changes to interview.rb

--- interview.rb
+++ interview.rb
@@ -1,11 +1,12 @@
 require 'json' 
+require 'date'
 
 class FreeSlot
   attr_accessor :start_time, :end_time
   
   def initialize(start_time, end_time)
-    self.start_time = DateTime.parse start_time
-    self.end_time = DateTime.parse end_time
+    self.start_time = (DateTime.parse start_time).strftime('%s')
+    self.end_time = (DateTime.parse end_time).strftime('%s')
   end
 end
 
@@ -49,33 +50,50 @@ ical_json = [
 ]
 
 gcal = JSON.parse gcal_json.to_json
-gcal.map{|i| i.strftime('%s')}
+# gcal.map{|i| i.strftime('%s')}
 # gcal << FreeSlot.new("2015-11-01T10:00:00.00+08:00", "2015-11-01T11:00:00.00+08:00")
+for elem in gcal 
+  elem['start'] = (DateTime.parse elem['start']).strftime('%s').to_i
+  elem['end'] = (DateTime.parse elem['end']).strftime('%s').to_i
+end
+
 
 ical = JSON.parse ical_json.to_json
+puts "<<<<<<<<#{ical}>>>>>>>"
+
+for elem in ical 
+  elem['start'] = (DateTime.parse elem['start']).strftime('%s').to_i
+  elem['end'] = (DateTime.parse elem['end']).strftime('%s').to_i
+end
 puts ical
-ical.map{|i| i.strftime('%s')}
 # gcal = [[12, 13], [13, 14], [14, 15], [15, 16]]
 # ical = [[10, 11], [11, 12, 13, 14], [15, 16, 17]]
 
-union_arr = (gcal + ical).sort_by!(&:length)
+union_arr = (gcal + ical).sort_by!{|elem| elem['end'] - elem['start']}
+
+puts "*********#{union_arr}*********"
 
 result = Array.new
 
-while(union_arr.last.length > 2)
+while(union_arr.length > 2)
   result << union_arr.last
-  puts "<<<<<<<< #{union_arr}>>>>>>>>>>"
-  union_arr.delete result.last
+  union_arr.pop
+  # puts "<<<<<<<< #{union_arr}>>>>>>>>>>"
   
-  for elem in result.last[0..result.last.size-2]
-    union_arr.each{|x|
-  	  if x.size== 2 && x[0]==elem
-  	    union_arr.delete(x)
-  	  end
-    }
-    puts "<<<<<<<#{union_arr}>>>>>>>>>"
-    puts "<<<<<<<#{result}>>>>>>>>>>>"
-  end
+  indexes = []
+  union_arr.each_with_index{|slot, i|
+	  if slot['end']-slot['start'] == 3600 && slot['start']>=result.last['start'] && slot['end']<= result.last['end']
+	    indexes << i
+      # puts i
+    end
+  }
+  puts indexes
+  indexes.each{|i| union_arr.delete_at i}
+    puts "<<<<<union<<#{union_arr}>>>>>>>>>"
+    puts "<<<result<<<<#{result}>>>>>>>>>>>"
 end
+
+union_arr.map!{|elem| {'start': DateTime.strptime(elem['start'].to_s, '%s').to_s, 'end': DateTime.strptime(elem['end'].to_s, '%s')}}
+result.map!{|elem| {'start': DateTime.strptime(elem['start'].to_s, '%s').to_s, 'end': DateTime.strptime(elem['end'].to_s, '%s')}}
 
 puts "<<<<<<#{union_arr + result}>>>>>>>>"
